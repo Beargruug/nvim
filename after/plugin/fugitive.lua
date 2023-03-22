5ce6eb9c8e8f99d44984676cfbfd679994964794
Fabian Scheid <f.scheid@evana.de>
2023-03-22 11:53:22 +0100

    adjust fugitive config

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,10 +1,10 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+local L9us_Fugitive = vim.api.nvim_create_augroup("L9us_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = ThePrimeagen_Fugitive,
+    group = L9us_Fugitive,
     pattern = "*",
     callback = function()
         if vim.bo.ft ~= "fugitive" then
