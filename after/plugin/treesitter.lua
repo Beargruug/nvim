c37508ab41a9edb71592ca8627244d3d982e3dba
Fabian Scheid <fabian@nuwork.de>
2023-07-12 11:38:55 +0200

    treesitter update

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,60 +2,35 @@ local function ts_disable(_, bufnr)
   return vim.api.nvim_buf_line_count(bufnr) > 5000
 end
 
-require('nvim-treesitter.configs').setup({
+require'nvim-treesitter.configs'.setup {
+  -- A list of parser names, or "all"
+  ensure_installed = { "vimdoc", "javascript", "typescript", "lua"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
   highlight = {
+    -- `false` will disable the whole extension
     enable = true,
     disable = function(lang, bufnr)
       return lang == "cmake" or ts_disable(lang, bufnr)
     end,
-    additional_vim_regex_highlighting = { "latex" },
-  },
-  context = {
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
-    throttle = true, -- Throttles plugin updates (may improve performance)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
-      -- For all filetypes
-      -- Note that setting an entry here replaces all other patterns for this entry.
-      -- By setting the 'default' entry below, you can control which nodes you want to
-      -- appear in the context window.
-      default = {
-        "function",
-        "method",
-        "for",
-        "while",
-        "if",
-        "switch",
-        "case",
-      },
-      typescript = {
-        "class_declaration",
-        "abstract_class_declaration",
-        "else_clause",
-      },
-    }
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
   },
   rainbow = {
     enable = true,
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
     max_file_lines = nil, -- Do not enable for files with more than n lines, int
   },
-  indent = {
-    enable = false,
-    disable = {},
-  },
-  autotag = {
-    enable = true,
-  },
-  ensure_installed = {
-    "tsx",
-    "javascript",
-    "typescript",
-    "lua",
-    "html",
-    "scss"
-  },
-})
+}
 
 local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
 parser_config.tsx.filetype_to_pasername = { "javascript", "typescript.tsx" }
