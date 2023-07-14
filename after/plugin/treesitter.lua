1ad9725d5d25dd6b4ca017625770fb8b30cb9818
Fabian Scheid <fabian@nuwork.de>
2023-07-14 09:17:09 +0200

    asf

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -25,6 +25,31 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+  context = {
+    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    throttle = true, -- Throttles plugin updates (may improve performance)
+    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+      -- For all filetypes
+      -- Note that setting an entry here replaces all other patterns for this entry.
+      -- By setting the 'default' entry below, you can control which nodes you want to
+      -- appear in the context window.
+      default = {
+        "function",
+        "method",
+        "for",
+        "while",
+        "if",
+        "switch",
+        "case",
+      },
+      typescript = {
+        "class_declaration",
+        "abstract_class_declaration",
+        "else_clause",
+      },
+    }
+  },
   rainbow = {
     enable = true,
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
