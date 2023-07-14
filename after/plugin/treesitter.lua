22cbc4e0a8d0f769a2eead533330e7f4b3c11eb2
Fabian Scheid <fabian@nuwork.de>
2023-07-14 09:23:50 +0200

    t

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -3,16 +3,6 @@ local function ts_disable(_, bufnr)
 end
 
 require'nvim-treesitter.configs'.setup {
-  -- A list of parser names, or "all"
-  ensure_installed = { "vimdoc", "javascript", "typescript", "lua"},
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
-  sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-  auto_install = true,
-
   highlight = {
     -- `false` will disable the whole extension
     enable = true,
@@ -25,6 +15,17 @@ require'nvim-treesitter.configs'.setup {
     -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
+
+  -- A list of parser names, or "all"
+  ensure_installed = { "vimdoc", "javascript", "typescript", "lua"},
+
+  -- Install parsers synchronously (only applied to `ensure_installed`)
+  sync_install = false,
+
+  -- Automatically install missing parsers when entering buffer
+  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+  auto_install = true,
+
   context = {
     enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
     throttle = true, -- Throttles plugin updates (may improve performance)
