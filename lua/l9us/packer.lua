f05edfd11d91572ab47628eee91f4b05cc56c811
Fabian Scheid <fabian@nuwork.de>
2023-07-17 09:09:40 +0200

    treesitter stuff

--- lua/l9us/packer.lua
+++ lua/l9us/packer.lua
@@ -41,7 +41,13 @@ return require('packer').startup(function(use)
   use('sheodox/projectlaunch.nvim')
 
   -- Treesitter
-  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
+  use {
+			'nvim-treesitter/nvim-treesitter',
+			run = function()
+				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+				ts_update()
+			end,}
+  use("nvim-treesitter/playground")
   use('nvim-treesitter/nvim-treesitter-context')
   use('eckon/treesitter-current-functions')
 
