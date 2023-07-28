dc2007b2d3ce3d554303aa2785e3816d6736d3f4
Fabian Scheid <fabian@nuwork.de>
2023-07-28 12:00:24 +0200

    remove comments

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -2,35 +2,22 @@ local function ts_disable(_, bufnr)
   return vim.api.nvim_buf_line_count(bufnr) > 5000
 end
 
-require'nvim-treesitter.configs'.setup {
+require 'nvim-treesitter.configs'.setup {
   highlight = {
-    -- `false` will disable the whole extension
     enable = true,
     disable = function(lang, bufnr)
       return lang == "cmake" or ts_disable(lang, bufnr)
     end,
-    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-    -- Using this option may slow down your editor, and you may see some duplicate highlights.
-    -- Instead of true it can also be a list of languages
     additional_vim_regex_highlighting = false,
   },
-
-  -- A list of parser names, or "all"
-  ensure_installed = { "vimdoc", "javascript", "typescript", "lua"},
-
-  -- Install parsers synchronously (only applied to `ensure_installed`)
+  ensure_installed = { "vimdoc", "javascript", "typescript", "lua" },
   sync_install = false,
-
-  -- Automatically install missing parsers when entering buffer
-  -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
   auto_install = true,
-
   context = {
-    enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
+    enable = true,   -- Enable this plugin (Can be enabled/disabled later via commands)
     throttle = true, -- Throttles plugin updates (may improve performance)
-    max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
-    patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
+    max_lines = 0,   -- How many lines the window should span. Values <= 0 mean no limit.
+    patterns = {     -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
       -- For all filetypes
       -- Note that setting an entry here replaces all other patterns for this entry.
       -- By setting the 'default' entry below, you can control which nodes you want to
@@ -54,9 +41,6 @@ require'nvim-treesitter.configs'.setup {
   rainbow = {
     enable = true,
     extended_mode = false, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
-    max_file_lines = nil, -- Do not enable for files with more than n lines, int
+    max_file_lines = nil,  -- Do not enable for files with more than n lines, int
   },
 }
-
-local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-parser_config.tsx.filetype_to_pasername = { "javascript", "typescript.tsx" }
--- lua/l9us/packer.lua
+++ lua/l9us/packer.lua
@@ -22,12 +22,12 @@ return require('packer').startup(function(use)
   use({
     "folke/trouble.nvim",
     config = function()
-        require("trouble").setup {
-            icons = false,
-            -- your configuration comes here
-            -- or leave it empty to use the default settings
-            -- refer to the configuration section below
-        }
+      require("trouble").setup {
+        icons = false,
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
     end
   })
 
@@ -41,12 +41,7 @@ return require('packer').startup(function(use)
   use('sheodox/projectlaunch.nvim')
 
   -- Treesitter
-  use {
-			'nvim-treesitter/nvim-treesitter',
-			run = function()
-				local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
-				ts_update()
-			end,}
+  use({ 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' })
   use("nvim-treesitter/playground")
   use('nvim-treesitter/nvim-treesitter-context')
 
