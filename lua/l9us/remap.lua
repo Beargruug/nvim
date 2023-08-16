e399921e5c3198571b24b153f00e6d4fe3230156
Fabian Scheid <fabian@nuwork.de>
2023-08-16 07:49:50 +0200

    add eslintfix to format cmd

--- lua/l9us/remap.lua
+++ lua/l9us/remap.lua
@@ -24,7 +24,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader><leader>f", vim.lsp.buf.format)
-vim.keymap.set("n", "<leader>f", "<cmd>Prettier<CR>")
+vim.keymap.set("n", "<leader>f", "<cmd>EslintFixAll<CR><cmd>Prettier<CR>")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
