93ff83abcfdca0494ff9736bcd057f6eea0d0161
Fabian Scheid <fabian@nuwork.de>
2023-09-08 13:06:15 +0200

    disable filetree

--- lua/l9us/remap.lua
+++ lua/l9us/remap.lua
@@ -4,7 +4,7 @@ vim.g.maplocalleader = ' '
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set("n", "sf", ":E<CR>")
+vim.keymap.set("n", "sf", ":Explore<CR>")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
