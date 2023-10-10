50bef9479c31fe61caf13260e9cb8787fbbb38fb
Fabian Scheid <fabian@nuwork.de>
2023-10-10 08:11:09 +0200

    update packages

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -4,28 +4,31 @@ local L9us_Fugitive = vim.api.nvim_create_augroup("L9us_Fugitive", {})
 
 local autocmd = vim.api.nvim_create_autocmd
 autocmd("BufWinEnter", {
-    group = L9us_Fugitive,
-    pattern = "*",
-    callback = function()
-        if vim.bo.ft ~= "fugitive" then
-            return
-        end
+  group = L9us_Fugitive,
+  pattern = "*",
+  callback = function()
+    if vim.bo.ft ~= "fugitive" then
+      return
+    end
 
-        local bufnr = vim.api.nvim_get_current_buf()
-        local opts = {buffer = bufnr, remap = false}
-        vim.keymap.set("n", "<leader>p", function()
-            vim.cmd.Git('push')
-        end, opts)
+    local bufnr = vim.api.nvim_get_current_buf()
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>p", function()
+      vim.cmd.Git('push')
+    end, opts)
 
-        -- rebase always
-        vim.keymap.set("n", "<leader>P", function()
-            vim.cmd.Git({'pull',  '--rebase'})
-        end, opts)
+    -- rebase always
+    vim.keymap.set("n", "<leader>P", function()
+      vim.cmd.Git({ 'pull', '--rebase' })
+    end, opts)
 
-        -- NOTE: It allows me to easily set the branch i am pushing and any tracking
-        -- needed if i did not set the branch up correctly
-        vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-    end,
+    -- commit message amend
+    vim.keymap.set("n", "<leader>C", ":Git commit --amend --no-edit", opts);
+
+    -- NOTE: It allows me to easily set the branch i am pushing and any tracking
+    -- needed if i did not set the branch up correctly
+    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
+  end,
 })
 
 vim.keymap.set("n", "gu", "<cmd>diffget //2<CR>")
