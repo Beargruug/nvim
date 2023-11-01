c5b10ab91aba1358d73d37fad0a7ac805f0b2af1
Fabian Scheid <fabian@nuwork.de>
2023-11-01 15:40:01 +0100

    change keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,8 +2,8 @@ local telescope = require('telescope')
 
 local builtin = require('telescope.builtin')
 
-vim.keymap.set('n', ';f', builtin.find_files, {})
-vim.keymap.set('n', '<c-p>', builtin.git_files, {})
+vim.keymap.set('n', ';f', builtin.git_files, {})
+vim.keymap.set('n', '<c-p>', builtin.find_files, {})
 vim.keymap.set('n', ';r', builtin.live_grep, {})
 vim.keymap.set('n', ';h', builtin.oldfiles, {})
 vim.keymap.set('n', '\\', builtin.buffers, {})
