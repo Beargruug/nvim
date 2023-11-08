911ae90f5018004218d95890dba32a24341b2017
Fabian Scheid <fabian@nuwork.de>
2023-11-08 11:17:36 +0100

    change eslint fix remap

--- lua/l9us/remap.lua
+++ lua/l9us/remap.lua
@@ -26,7 +26,7 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
--- vim.keymap.set("n", "<leader>f", "<cmd>EslintFixAll<CR><cmd>Prettier<CR>")
+vim.keymap.set("n", "<leader>e", "<cmd>EslintFixAll<CR>")
 
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
