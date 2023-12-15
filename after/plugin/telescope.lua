32022f4147d77433ec72de20359be41a0e0afbdc
Fabian Scheid <fabian@nuwork.de>
2023-12-15 09:21:23 +0100

    add helptags keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -4,6 +4,7 @@ local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', ';f', builtin.git_files, {})
 vim.keymap.set('n', '<c-p>', builtin.find_files, {})
+vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
 vim.keymap.set('n', ';r', builtin.live_grep, {})
 vim.keymap.set('n', ';h', builtin.oldfiles, {})
 vim.keymap.set('n', '\\', builtin.buffers, {})
