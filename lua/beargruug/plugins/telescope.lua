c111357966b29e89c8615fff445b4a1ad3d46839
Fabian Scheid <fabian@nuwork.de>
2024-01-26 13:47:50 +0100

    live grep words under cursor

--- lua/beargruug/plugins/telescope.lua
+++ lua/beargruug/plugins/telescope.lua
@@ -3,29 +3,26 @@ return {
     'nvim-telescope/telescope.nvim',
     requires = {
       'nvim-lua/plenary.nvim',
-      'nvim-lua/popup.nvim',
     },
     config = function()
-      local actions = require('telescope.actions')
-      local builtin = require('telescope.builtin')
+      require('telescope').setup({})
 
+      local builtin = require('telescope.builtin')
       vim.keymap.set('n', ';f', builtin.git_files, {})
       vim.keymap.set('n', '<c-p>', builtin.find_files, {})
       vim.keymap.set('n', '<leader>vh', builtin.help_tags, {})
       vim.keymap.set('n', ';r', builtin.live_grep, {})
       vim.keymap.set('n', ';h', builtin.oldfiles, {})
       vim.keymap.set('n', '\\', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>pws', function()
+        local word = vim.fn.expand('<cword>')
+        builtin.grep_string({ search = word })
+      end)
+      vim.keymap.set('n', '<leader>pWs', function()
+        local word = vim.fn.expand('<cWORD>')
+        builtin.grep_string({ search = word })
+      end)
 
-      require('telescope').setup({
-        defaults = {
-          file_ignore_patterns = { "node_modules" },
-          mappings = {
-            n = {
-              ["q"] = actions.close
-            },
-          },
-        },
-      })
       vim.keymap.set('n', '<leader>gwt', ':Telescope git_worktree git_worktrees<CR>')
       vim.keymap.set('n', '<leader>gct', ':Telescope git_worktree create_git_worktree<CR>')
       vim.keymap.set('n', '<leader>gdt',
