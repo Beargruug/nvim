49f0e66124e4f71a96bcbec193b988b58a412625
Fabian Scheid <fabian@nuwork.de>
2024-01-26 18:11:50 +0100

    trouble

--- lua/beargruug/plugins/trouble.lua
+++ lua/beargruug/plugins/trouble.lua
@@ -2,26 +2,28 @@ return {
   {
     "folke/trouble.nvim",
     config = function()
-      require("trouble").setup {
+      local trouble = require('trouble')
+
+      trouble.setup {
         icons = false,
       }
       vim.keymap.set("n", "<leader>tt", function()
-        require("trouble").toggle()
+        trouble.toggle()
       end)
       vim.keymap.set("n", "<leader>xw", function()
-        require("trouble").toggle("workspace_diagnostics")
+        trouble.toggle("workspace_diagnostics")
       end)
       vim.keymap.set("n", "<leader>xd", function()
-        require("trouble").toggle("document_diagnostics")
+        trouble.toggle("document_diagnostics")
       end)
       vim.keymap.set("n", "<leader>xq", function()
-        require("trouble").toggle("quickfix")
+        trouble.toggle("quickfix")
       end)
       vim.keymap.set("n", "<leader>xl", function()
-        require("trouble").toggle("loclist")
+        trouble.toggle("loclist")
       end)
       vim.keymap.set("n", "gR", function()
-        require("trouble").toggle("lsp_references")
+        trouble.toggle("lsp_references")
       end)
     end
   },
