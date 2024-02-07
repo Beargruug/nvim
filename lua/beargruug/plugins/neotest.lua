e10855b67e7a34d4edc55a9b3c0c7ac85368cf3e
Fabian Scheid <fabian@nuwork.de>
2024-02-07 07:22:11 +0100

    add neotest for testing inside of nvim - awesome

---
+++ lua/beargruug/plugins/neotest.lua
@@ -0,0 +1,30 @@
+return {
+  {
+    "nvim-neotest/neotest",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "antoinemadec/FixCursorHold.nvim",
+      "nvim-treesitter/nvim-treesitter",
+      "marilari88/neotest-vitest",
+      "nvim-neotest/neotest-plenary",
+    },
+    config = function()
+      local neotest = require("neotest")
+      neotest.setup({
+        adapters = {
+          require("neotest-vitest"),
+          require("neotest-plenary"),
+          require("neotest-playwright"),
+        }
+      })
+
+      vim.keymap.set("n", "<leader>tc", function()
+        neotest.run.run()
+      end)
+
+      vim.keymap.set("n", "<leader>tf", function()
+        neotest.run.run(vim.fn.expand("%"))
+      end)
+    end,
+  },
+}
