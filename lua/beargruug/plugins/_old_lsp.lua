684b33d7bbcdc2f2ea7c0117b2b66b5b1301ad92
Fabian Scheid <fabian@nuwork.de>
2024-02-07 07:00:34 +0100

    update lsp

---
+++ lua/beargruug/plugins/_old_lsp.lua
@@ -0,0 +1,91 @@
+local function attach(opts)
+  vim.keymap.set('n', 'gr', '<cmd>Telescope lsp_references<cr>', opts)
+  vim.keymap.set('n', 'gd', '<cmd>Telescope lsp_definitions<cr>', opts)
+  vim.keymap.set('n', 'gi', '<cmd>Telescope lsp_implementations<cr>', opts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  vim.keymap.set('n', '<leader>vws', vim.lsp.buf.workspace_symbol, opts)
+  vim.keymap.set('n', '<leader>vd', vim.diagnostic.open_float, opts)
+  vim.keymap.set('n', '[d', vim.diagnostic.goto_next, opts)
+  vim.keymap.set('n', ']d', vim.diagnostic.goto_prev, opts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
+  vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, opts)
+  vim.keymap.set('n', '<leader>e', '<cmd>EslintFixAll<cr>', opts)
+end
+
+return {
+  { 'folke/lsp-colors.nvim',
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      dependencies = {
+        -- LSP Support
+        { 'neovim/nvim-lspconfig' },
+        { 'williamboman/mason.nvim' },
+        { 'williamboman/mason-lspconfig.nvim' },
+
+        -- Autocompletion
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'hrsh7th/cmp-buffer' },
+        { 'hrsh7th/cmp-path' },
+        { 'hrsh7th/cmp-cmdline' },
+        { 'hrsh7th/nvim-cmp' },
+        { 'saadparwaiz1/cmp_luasnip' },
+        { 'hrsh7th/cmp-nvim-lua' },
+
+        -- Snippets
+        { 'L3MON4D3/LuaSnip' },
+        { 'rafamadriz/friendly-snippets' },
+      },
+      config = function()
+        local lsp = require("lsp-zero")
+
+        lsp.preset("recommended")
+
+        lsp.ensure_installed({
+          'tsserver',
+          'eslint',
+        })
+
+        local cmp = require('cmp')
+        local cmp_select = { behavior = cmp.SelectBehavior.Select }
+        local cmp_mappings = lsp.defaults.cmp_mappings({
+          ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
+          ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
+          ['<CR>'] = cmp.mapping.confirm({ select = true }),
+          ['<C-Space>'] = cmp.mapping.complete(),
+        })
+
+        -- disable completion with tab
+        -- this helps with copilot setup
+        cmp_mappings['<Tab>'] = nil
+        cmp_mappings['<S-Tab>'] = nil
+
+        lsp.setup_nvim_cmp({
+          mapping = cmp_mappings
+        })
+
+        lsp.set_preferences({
+          suggest_lsp_servers = false,
+          sign_icons = {
+            error = 'E',
+            warn = 'W',
+            hint = 'H',
+            info = 'I'
+          }
+        })
+
+        lsp.on_attach(function(_, bufnr)
+          local opts = { buffer = bufnr, remap = false }
+          attach(opts)
+        end)
+
+        lsp.setup()
+
+        vim.diagnostic.config({
+          virtual_text = true,
+        })
+      end
+    },
+  },
+}
