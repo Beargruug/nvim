d3949172c6f6766de3c2bcfb1788112ec0afdcaa
Beargruug <fabian.scheid@pocket-rocket.io>
2024-10-17 11:00:00 +0200

    update tree on file focus

--- lua/beargruug/plugins/tree.lua
+++ lua/beargruug/plugins/tree.lua
@@ -4,6 +4,9 @@ return {
 		require("nvim-tree").setup({
 			disable_netrw = false,
 			hijack_netrw = false,
+			update_focused_file = {
+				enable = true,
+			},
 			view = {
 				width = 50,
 			},
