91f4586a89c60e01e8d234a7284996274c195c62
Fabian Scheid <fabian.scheid@pocket-rocket.io>
2025-01-17 08:25:15 +0100

    undooooo

--- lua/beargruug/plugins/fugitive.lua
+++ lua/beargruug/plugins/fugitive.lua
@@ -17,7 +17,9 @@ return {
 
           local bufnr = vim.api.nvim_get_current_buf()
           local opts = { buffer = bufnr, remap = false }
-          vim.keymap.set("n", "<leader>p", ":Git push <cr>", opts)
+          vim.keymap.set("n", "<leader>p", function()
+            vim.cmd.Git('push')
+          end, opts)
 
           -- rebase always
           vim.keymap.set("n", "<leader>P", ":Git pull --rebase <cr>", opts)
