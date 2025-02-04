3bc778da942b248648f53f4dc33a53ab24031aac
Fabian Scheid <fabian.scheid@pocket-rocket.io>
2025-02-04 08:42:11 +0100

    adjust remap for files

--- lua/beargruug/plugins/telescope.lua
+++ lua/beargruug/plugins/telescope.lua
@@ -19,12 +19,12 @@ return {
 
 			local builtin = require("telescope.builtin")
 			-- vim.keymap.set("n", ";f", builtin.git_files, {})
-			vim.keymap.set("n", ";f", builtin.find_files, {})
+			vim.keymap.set("n", "<c-P>", builtin.find_files, {})
 			vim.keymap.set("n", "<leader>vh", builtin.help_tags, {})
-			vim.keymap.set("n", ";r", builtin.live_grep, {})
-			vim.keymap.set("n", "<c-P>", builtin.oldfiles, {})
+			vim.keymap.set("n", "<leader>pr", builtin.live_grep, {})
+			vim.keymap.set("n", "<leader>of", builtin.oldfiles, {})
 			-- this is awesome
-			vim.keymap.set("n", "<space>/", builtin.current_buffer_fuzzy_find)
+			vim.keymap.set("n", "<leader>/", builtin.current_buffer_fuzzy_find)
 			--
 			vim.keymap.set("n", "<leader>b", builtin.buffers, {})
 			-- vim.keymap.set("n", "<leader>pws", function()
