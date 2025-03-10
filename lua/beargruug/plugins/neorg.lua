781c57f999ae0960d1aab4c86808c2312f0f05bc
Fabian Scheid <fabian.scheid@pocket-rocket.io>
2025-03-10 11:28:56 +0100

    add neorg for note taking test

---
+++ lua/beargruug/plugins/neorg.lua
@@ -0,0 +1,22 @@
+return {
+	"nvim-neorg/neorg",
+	lazy = false,
+	version = "*",
+	config = function()
+		local neorg = require("neorg")
+		neorg.setup({
+			load = {
+				["core.defaults"] = {},
+				["core.dirman"] = {
+					config = {
+						autochdir = true,
+						workspaces = {
+							default = "~/.dotfiles/personal/neorg",
+						},
+						index = "index.norg",
+					},
+				},
+			},
+		})
+	end,
+}
