8110257d482a7226f0c4f4195afc82cc6ed5ba3e
Fabian Scheid <fabian.scheid@pocket-rocket.io>
2025-05-09 10:02:37 +0200

    update width of zenmode

--- lua/beargruug/plugins/zenmode.lua
+++ lua/beargruug/plugins/zenmode.lua
@@ -4,7 +4,7 @@ return {
     config = function()
       require("zen-mode").setup({
         window = {
-          width = 90,
+          width = 150,
           options = {
             number = true,
             relativenumber = true,
