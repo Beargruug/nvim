813f6058315e5a362aeb0813799dec0cb3f0abc1
Fabian Scheid <fabian.scheid@pocket-rocket.io>
2025-05-13 09:03:01 +0200

    remove unused terminal and add go shortcuts

--- after/ftplugin/go.lua
+++ after/ftplugin/go.lua
@@ -4,6 +4,13 @@ set.expandtab = false
 set.tabstop = 4
 set.shiftwidth = 4
 
+vim.keymap.set("n", "<leader>ee", "oif err != nil {<CR>}<Esc>Oreturn err<Esc>")
+
+vim.keymap.set("n", "<leader>ea", 'oassert.NoError(err, "")<Esc>F";a')
+
+vim.keymap.set("n", "<leader>ef", 'oif err != nil {<CR>}<Esc>Olog.Fatalf("error: %s\\n", err.Error())<Esc>jj')
+
+vim.keymap.set("n", "<leader>el", 'oif err != nil {<CR>}<Esc>O.logger.Error("error", "error", err)<Esc>F.;i')
 -- vim.keymap.set("n", "<space>td", function()
 --       require("dap-go").debug_test()
 --   end, { buffer = 0 })
