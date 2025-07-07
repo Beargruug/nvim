b41ceeb301b4aa7368e5ecf5bcf4d5295b99a050
Fabian Scheid <info@fabianmail.com>
2025-07-07 07:16:53 +0200

    update & change keybindings for ruby files

--- after/ftplugin/ruby.lua
+++ after/ftplugin/ruby.lua
@@ -1,2 +1,2 @@
-vim.keymap.set("n", "<leader>l", ":put =printf('pp %s', expand('<cword>'))<CR>")
+vim.keymap.set("n", "<leader>ll", ":put =printf('pp %s', expand('<cword>'))<CR>")
 vim.keymap.set("n", "<leader>lp", ":put =printf('puts(''%s:'', %s);', expand('<cword>'), expand('<cword>'))<CR>")
