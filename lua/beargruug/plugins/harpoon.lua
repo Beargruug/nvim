0cab76253a05f25b7e84cf58c17157d0a44e228a
Fabian Scheid <info@fabianmail.com>
2025-07-14 10:38:24 +0200

    update keymaps for harpoon

--- lua/beargruug/plugins/harpoon.lua
+++ lua/beargruug/plugins/harpoon.lua
@@ -10,9 +10,10 @@ return {
       vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
       vim.keymap.set("n", "<c-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
-      vim.keymap.set("n", "<c-b>", function() harpoon:list():select(1) end)
-      vim.keymap.set("n", "<c-n>", function() harpoon:list():select(2) end)
-      vim.keymap.set("n", "<c-m>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<c-t>", function() harpoon:list():select(1) end)
+      vim.keymap.set("n", "<c-g>", function() harpoon:list():select(2) end)
+      vim.keymap.set("n", "<c-b>", function() harpoon:list():select(3) end)
+      vim.keymap.set("n", "<c-n>", function() harpoon:list():select(4) end)
     end
   },
 }
