3d7f03658e7abf3216566c0ba9f8cb0bee36f1b3
Fabian Scheid <info@fabianmail.com>
2025-07-14 11:27:07 +0200

    themes.. should i switch?

--- lua/beargruug/plugins/colorscheme.lua
+++ lua/beargruug/plugins/colorscheme.lua
@@ -17,14 +17,9 @@ return {
 			dimInactive = false, -- dim inactive window `:h hl-NormalNC`
 			globalStatus = false, -- adjust window separators highlight for laststatus=3
 			terminalColors = true, -- define vim.g.terminal_color_{0,17}
-			theme = "wave", -- Load "wave" theme when 'background' option is not set
-			background = { -- map the value of 'background' option to a theme
-				dark = "wave", -- try "dragon" !
-				light = "lotus",
-			},
 		})
 
-		vim.cmd("colorscheme kanagawa")
+		vim.cmd("colorscheme kanagawa-wave")
 	end,
 },
 }
