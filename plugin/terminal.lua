ddf07dd711b280d2b776c4a82c1ecf4598a0a617
Fabian Scheid <info@fabianmail.com>
2025-07-24 14:42:24 +0200

    update terminal and move terminal to top of the screen + adjust height

--- plugin/terminal.lua
+++ plugin/terminal.lua
@@ -1,27 +1,27 @@
 local ship_cmd_id = 0
 
 vim.api.nvim_create_autocmd("TermOpen", {
-	group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
-	pattern = "*",
-	callback = function()
-		vim.opt.number = false
-		vim.opt.relativenumber = false
-	end,
+    group = vim.api.nvim_create_augroup("custom-term-open", { clear = true }),
+    pattern = "*",
+    callback = function()
+        vim.opt.number = false
+        vim.opt.relativenumber = false
+    end,
 })
 
 local toggle_small_terminal = function()
-	vim.cmd.vnew()
-	vim.cmd.term()
-	vim.cmd.wincmd("J")
-	vim.api.nvim_win_set_height(0, 10)
+    vim.cmd.vnew()
+    vim.cmd.term()
+    vim.cmd.wincmd("K")
+    vim.api.nvim_win_set_height(0, 20)
 
-	ship_cmd_id = vim.bo.channel
+    ship_cmd_id = vim.bo.channel
 end
 
 local ship_config_code = function()
-	vim.fn.chansend(ship_cmd_id, { "git add . \r\n" })
-	vim.fn.chansend(ship_cmd_id, { "git commit -m 'update submodules' \r\n" })
-	vim.fn.chansend(ship_cmd_id, { "git push origin main \r\n" })
+    vim.fn.chansend(ship_cmd_id, { "git add . \r\n" })
+    vim.fn.chansend(ship_cmd_id, { "git commit -m 'update submodules' \r\n" })
+    vim.fn.chansend(ship_cmd_id, { "git push origin main \r\n" })
 end
 
 vim.keymap.set("t", "<esc><esc>", "<c-\\><c-n>")
