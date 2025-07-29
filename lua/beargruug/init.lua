a2bad1f8d6bacf7d059564eebb37c56d276d127e
Fabian Scheid <info@fabianmail.com>
2025-07-29 08:35:34 +0200

    add references to lsp

--- lua/beargruug/init.lua
+++ lua/beargruug/init.lua
@@ -37,6 +37,7 @@ autocmd("LspAttach", {
         vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
         vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
         vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
         vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
         vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
