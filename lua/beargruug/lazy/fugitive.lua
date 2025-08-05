d003b17520ba06da013d800f986c751d93ea5661
Fabian Scheid <info@fabianmail.com>
2025-08-05 11:00:29 +0200

    Automated dotfiles update (#5)

--- lua/beargruug/lazy/fugitive.lua
+++ lua/beargruug/lazy/fugitive.lua
@@ -1,36 +1,35 @@
 return {
-    {
-        'tpope/vim-fugitive',
-        config = function()
-            vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-            local Beargruug_Fugitive = vim.api.nvim_create_augroup("Beargruug_Fugitive", {})
+			local Beargruug_Fugitive = vim.api.nvim_create_augroup("Beargruug_Fugitive", {})
 
-            local autocmd = vim.api.nvim_create_autocmd
-            autocmd("BufWinEnter", {
-                group = Beargruug_Fugitive,
-                pattern = "*",
-                callback = function()
-                    if vim.bo.ft ~= "fugitive" then
-                        return
-                    end
+			local autocmd = vim.api.nvim_create_autocmd
+			autocmd("BufWinEnter", {
+				group = Beargruug_Fugitive,
+				pattern = "*",
+				callback = function()
+					if vim.bo.ft ~= "fugitive" then
+						return
+					end
 
-                    local bufnr = vim.api.nvim_get_current_buf()
-                    local opts = { buffer = bufnr, remap = false }
-                    vim.keymap.set("n", "<leader>p", function()
-                        vim.cmd.Git('push')
-                    end, opts)
+					local bufnr = vim.api.nvim_get_current_buf()
+					local opts = { buffer = bufnr, remap = false }
+					vim.keymap.set("n", "<leader>p", function()
+						vim.cmd.Git("push")
+					end, opts)
 
-                    -- rebase always
-                    vim.keymap.set("n", "<leader>P", ":Git pull --rebase <cr>", opts)
+					-- rebase always
+					vim.keymap.set("n", "<leader>P", ":Git pull --rebase <cr>", opts)
 
-                    vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts);
-                end,
-            })
+					vim.keymap.set("n", "<leader>t", ":Git push -u origin ", opts)
+				end,
+			})
 
-            vim.keymap.set("n", "gt", "<cmd>diffget //2<CR>")
-            vim.keymap.set("n", "gu", "<cmd>diffget //3<CR>")
-        end
-    }
-
+			vim.keymap.set("n", "gt", "<cmd>diffget //2<CR>")
+			vim.keymap.set("n", "gu", "<cmd>diffget //3<CR>")
+		end,
+	},
 }
