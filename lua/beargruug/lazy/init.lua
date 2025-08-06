7c2aea1bf1e08c99dfef87ac4889226eaf36d851
Fabian Scheid <info@fabianmail.com>
2025-08-06 09:43:36 +0200

    Automated dotfiles update

--- lua/beargruug/lazy/init.lua
+++ lua/beargruug/lazy/init.lua
@@ -1,31 +1,31 @@
 return {
-    { "nvim-lua/plenary.nvim" },
-    { "tpope/vim-repeat" },
-    { "tpope/vim-surround" },
+	{ "nvim-lua/plenary.nvim" },
+	{ "tpope/vim-repeat" },
+	{ "tpope/vim-surround" },
 
-    { "ThePrimeagen/git-worktree.nvim" },
-    { "ThePrimeagen/vim-be-good" },
+	{ "ThePrimeagen/git-worktree.nvim" },
+	{ "ThePrimeagen/vim-be-good" },
 
-    { "nvim-treesitter/playground" },
-    {
-        "thenbe/markdown-todo.nvim",
-        ft = { "md", "markdown" },
-        config = true,
-    },
+	{ "nvim-treesitter/playground" },
+	{
+		"thenbe/markdown-todo.nvim",
+		ft = { "md", "markdown" },
+		config = true,
+	},
 
-    {
-        "numToStr/Comment.nvim",
-        config = function()
-            require("Comment").setup()
-        end,
-    },
-    {
-        "sindrets/diffview.nvim",
-        dependencies = { "nvim-tree/nvim-web-devicons" },
-    },
+	{
+		"numToStr/Comment.nvim",
+		config = function()
+			require("Comment").setup()
+		end,
+	},
+	{
+		"sindrets/diffview.nvim",
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+	},
 
-    -- { "github/copilot.vim" },
-    {
-        dir = "~/personal/skipper.nvim",
-    },
+	-- { "github/copilot.vim" },
+	{
+		dir = "~/personal/skipper.nvim",
+	},
 }
