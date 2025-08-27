c2b7b01721ba48e4a524d385439183eb1f8463fb
Fabian Scheid <info@fabianmail.com>
2025-08-27 13:29:04 +0200

    Automated dotfiles update

--- lua/beargruug/init.lua
+++ lua/beargruug/init.lua
@@ -31,13 +31,12 @@ autocmd("LspAttach", {
 	group = BeargruugGroup,
 	callback = function(e)
 		local opts = { buffer = e.buffer }
-		vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
-		vim.keymap.set("n", "gd", "<cmd>Telescope lsp_definitions<cr>", opts)
+		-- vim.keymap.set("n", "gr", "<cmd>Telescope lsp_references<cr>", opts)
+		vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
 		vim.keymap.set("n", "gi", "<cmd>Telescope lsp_implementations<cr>", opts)
 		vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
 		vim.keymap.set("n", "<leader>vws", vim.lsp.buf.workspace_symbol, opts)
 		vim.keymap.set("n", "<leader>vd", vim.diagnostic.open_float, opts)
-		vim.keymap.set("n", "<leader>vrr", vim.lsp.buf.references, opts)
 		vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
 		vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
 		vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
