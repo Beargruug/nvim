26bc24825d3364eccbaf9f08e2be643bb1ba1541
Fabian Scheid <info@fabianmail.com>
2025-09-02 08:02:28 +0200

    Automated dotfiles update

---
+++ lua/beargruug/lazy/fff.lua
@@ -0,0 +1,31 @@
+return {
+	"dmtrKovalenko/fff.nvim",
+	build = "cargo build --release",
+	opts = { -- (optional)
+		debug = {
+			enabled = true, -- we expect your collaboration at least during the beta
+			show_scores = true, -- to help us optimize the scoring system, feel free to share your scores!
+		},
+	},
+	lazy = false,
+	keys = {
+		{
+			"ff", -- try it if you didn't it is a banger keybinding for a picker
+			function()
+				require("fff").find_files()
+			end,
+			desc = "FFFind files",
+		},
+	},
+	config = function()
+		local fff = require("fff")
+		fff.setup({
+			prompt = "",
+			preview = {
+				line_numbers = false,
+				wrap_lines = false,
+				show_file_info = false,
+			},
+		})
+	end,
+}
--- lua/beargruug/lazy/oil.lua
+++ lua/beargruug/lazy/oil.lua
@@ -1,47 +1,47 @@
 return {
-    "stevearc/oil.nvim",
-    -- Optional dependencies
-    dependencies = { { "echasnovski/mini.icons", opts = {} } },
-    -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
-    config = function()
-        local oil = require("oil")
+	"stevearc/oil.nvim",
+	-- Optional dependencies
+	dependencies = { { "echasnovski/mini.icons", opts = {} } },
+	-- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+	config = function()
+		local oil = require("oil")
 
-        oil.setup({
-            view_options = {
-                show_hidden = true,
-            },
-            keymaps = {
-                ["g?"] = "actions.show_help",
-                ["<CR>"] = "actions.select",
-                ["<C-s>"] = {
-                    "actions.select",
-                    opts = { vertical = true },
-                    desc = "Open the entry in a vertical split",
-                },
-                ["<C-h>"] = {
-                    "actions.select",
-                    opts = { horizontal = true },
-                    desc = "Open the entry in a horizontal split",
-                },
-                ["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
-                ["<C-c>"] = "actions.close",
-                ["<C-l>"] = "actions.refresh",
-                ["<C-p>"] = false,
-                ["-"] = "actions.parent",
-                ["_"] = "actions.open_cwd",
-                ["`"] = "actions.cd",
-                ["~"] = {
-                    "actions.cd",
-                    opts = { scope = "tab" },
-                    desc = ":tcd to the current oil directory",
-                    mode = "n",
-                },
-                ["gs"] = "actions.change_sort",
-                ["gx"] = "actions.open_external",
-                ["g."] = "actions.toggle_hidden",
-                ["g\\"] = "actions.toggle_trash",
-            },
-        })
-        vim.keymap.set("n", "<leader>sf", "<cmd>Oil<CR>")
-    end,
+		oil.setup({
+			view_options = {
+				show_hidden = true,
+			},
+			keymaps = {
+				["g?"] = "actions.show_help",
+				["<CR>"] = "actions.select",
+				["<C-s>"] = {
+					"actions.select",
+					opts = { vertical = true },
+					desc = "Open the entry in a vertical split",
+				},
+				["<C-h>"] = {
+					"actions.select",
+					opts = { horizontal = true },
+					desc = "Open the entry in a horizontal split",
+				},
+				["<C-t>"] = { "actions.select", opts = { tab = true }, desc = "Open the entry in new tab" },
+				["<C-c>"] = "actions.close",
+				["<C-l>"] = "actions.refresh",
+				["<C-p>"] = false,
+				["-"] = "actions.parent",
+				["_"] = "actions.open_cwd",
+				["`"] = "actions.cd",
+				["~"] = {
+					"actions.cd",
+					opts = { scope = "tab" },
+					desc = ":tcd to the current oil directory",
+					mode = "n",
+				},
+				["gs"] = "actions.change_sort",
+				["gx"] = "actions.open_external",
+				["g."] = "actions.toggle_hidden",
+				["g\\"] = "actions.toggle_trash",
+			},
+		})
+		vim.keymap.set("n", "<leader>sf", "<cmd>Oil<CR>")
+	end,
 }
