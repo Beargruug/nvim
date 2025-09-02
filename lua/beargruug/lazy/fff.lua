48d2fa521c74dd8c512ce963476a1f835814127c
Fabian Scheid <info@fabianmail.com>
2025-09-02 08:07:34 +0200

    Automated dotfiles update (#37)

--- lua/beargruug/lazy/fff.lua
+++ lua/beargruug/lazy/fff.lua
@@ -10,7 +10,7 @@ return {
 	lazy = false,
 	keys = {
 		{
-			"ff", -- try it if you didn't it is a banger keybinding for a picker
+			"<leader>pf", -- try it if you didn't it is a banger keybinding for a picker
 			function()
 				require("fff").find_files()
 			end,
