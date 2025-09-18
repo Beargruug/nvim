0dd04cbd35ef42f913b1e3d0d46166626370dd1c
Fabian Scheid <info@fabianmail.com>
2025-09-18 09:05:31 +0200

    Automated dotfiles update (#49)

--- lua/beargruug/lazy/lsp.lua
+++ lua/beargruug/lazy/lsp.lua
@@ -37,13 +37,12 @@ return {
 			},
 			handlers = {
 				function(server_name)
-					require("lspconfig")[server_name].setup({
+					vim.lsp.config(server_name, {
 						capabilities = capabilities,
 					})
 				end,
 				["lua_ls"] = function()
-					local lspconfig = require("lspconfig")
-					lspconfig.lua_ls.setup({
+					vim.lsp.config("lua_ls", {
 						capabilities = capabilities,
 						settings = {
 							Lua = {
@@ -58,8 +57,8 @@ return {
 						},
 					})
 				end,
-                -- don't know why this syntax is needed
-				require("lspconfig").ruby_lsp.setup({
+				-- don't know why this syntax is needed
+				vim.lsp.config("ruby_lsp", {
 					init_options = {
 						enabledFeatures = {
 							diagnostics = false,
