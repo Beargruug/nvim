f404c9e62abf6b4852f117834ecd2f5aa3196b52
Fabian Scheid <info@fabianmail.com>
2025-11-20 08:55:20 +0100

    Automated dotfiles update

--- lua/beargruug/lazy/oil.lua
+++ lua/beargruug/lazy/oil.lua
@@ -43,5 +43,19 @@ return {
 			},
 		})
 		vim.keymap.set("n", "<leader>sf", "<cmd>Oil<CR>")
+		vim.api.nvim_create_autocmd("FileType", {
+			pattern = "oil",
+			callback = function()
+				vim.keymap.set("n", "<leader>cp", function()
+					local entry = oil.get_cursor_entry()
+					if entry then
+						local full_path = oil.get_current_dir() .. entry.name
+						local rel_path = vim.fn.fnamemodify(full_path, ":.")
+						vim.fn.setreg("+", rel_path)
+						print("Copied relative path: " .. rel_path)
+					end
+				end, { buffer = true, desc = "Copy Oil file path to clipboard" })
+			end,
+		})
 	end,
 }
