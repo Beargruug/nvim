15d172a3dd62425a982f30c800f1dc95bfe6bee5
Beargruug <info@fabianmail.com>
2026-05-20 10:34:24 +0200

    Automated dotfiles update

--- lua/beargruug/remap.lua
+++ lua/beargruug/remap.lua
@@ -15,7 +15,7 @@ vim.keymap.set("n", "<leader>zl", "<cmd>LspRestart<cr>")
 vim.keymap.set("x", "<leader>p", [["_dP]])
 
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
-vim.keymap.set("v", "<leader>Y", '"+Y', { noremap = true })
+vim.keymap.set({ "n", "v" }, "<leader>Y", '"+Y', { noremap = true })
 
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
