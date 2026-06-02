27b5f22e9bdcea382d4bc5a9e002101f6810affe
Beargruug <info@fabianmail.com>
2026-06-02 11:19:04 +0200

    Automated dotfiles update

--- lua/beargruug/lazy/hardtime.lua
+++ lua/beargruug/lazy/hardtime.lua
@@ -1,8 +1,24 @@
 return {
 	"m4xshen/hardtime.nvim",
 	lazy = false,
-	dependencies = { "MunifTanjim/nui.nvim" },
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		{
+			"j-hui/fidget.nvim",
+			opts = {
+				notification = {
+					window = {
+						winblend = 0,
+						relative = "editor",
+					},
+				},
+			},
+		},
+	},
 	opts = {
 		disable_mouse = false,
+		callback = function(text)
+			require("fidget").notify(text)
+		end,
 	},
 }
